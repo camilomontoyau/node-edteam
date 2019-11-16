287824ba2c92f5c818e2490665362ba27dc84b2d
camilo <camilomontoya2@gmail.com>
2019-11-16 06:45:31 -0500

    servidor https escuchando

--- index.js
+++ index.js
@@ -2,12 +2,20 @@
 
 //dependencias
 const http = require('http');
+const https = require('https');
 const servidorUnificado = require('./servidorunificado');
 
 //crear el servidor
 const servidor = http.createServer(servidorUnificado);
 
+// servidor https
+const servidorHttps = https.createServer(servidorUnificado);
+
 //el servidor debe mantener el proceso y escuchar peticiones http
 servidor.listen(3000, () => {
-  console.log('El servidor está escuchando en el puerto 3000');
+  console.log('El servidor http está escuchando en el puerto 3000');
+});
+
+servidorHttps.listen(3001, () => {
+  console.log('El servidor https está escuchando en el puerto 3001');
 });
