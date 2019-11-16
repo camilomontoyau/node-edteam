50582f14f019a9d0d847d8c2599ff7605909a69c
camilo <camilomontoya2@gmail.com>
2019-11-16 04:40:14 -0500

    dependencias y crear el servidor

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hola mundo');
+/* archivo principal de nuestra app */
+
+//dependencias
+const http = require('http');
+
+//crear el servidor
+const servidor = http.createServer((req, res) => {
+  res.end();
+});
